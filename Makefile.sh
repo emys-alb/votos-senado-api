beb2267a4e3609860c095c801d6afec5b2575ae6
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2024-02-11 17:39:44 -0300

    Finish makefile

--- Makefile.sh
+++ Makefile.sh
@@ -4,26 +4,17 @@
 git clone "https://github.com/emys-alb/votos-senado"
 cd votos-senado/
 
-pipenv shell
-pipenv install
-
 # get date time
 datetime=$(date '+%m_%d_%Y_%H_%M_%S')
 today=$(date "+%d/%m/%Y")
 last_monday=$(date -d 'last-monday' '+%d/%m/%Y')
-filename="novosvotos.csv"
+filename="votos.csv"
 
 #define and update enviroment variable
-cp .env.sample .env
 echo -e FILENAME=$filename '\n'INIT_DATE=$last_monday '\n'FINISH_DATE=$today > .env
 
+pipenv shell
+pipenv install
+
 #run the spider
-cd scrapper/
-python paginas.py
-
-#check if new file is empty
-cd ../out/
-
-if [ ! -s "$filename" ]; 
-then rm $filename
-fi
+cd scrapper/ & python paginas.py
